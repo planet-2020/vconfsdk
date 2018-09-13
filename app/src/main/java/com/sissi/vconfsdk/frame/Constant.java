9c027d845c3ed5fbcb9df4421249674e226c5651
xingbiyanshu <xingbiyanshu@gmail.com>
2018-09-13 13:12:10 +0800

    optimize

---
+++ app/src/main/java/com/sissi/vconfsdk/frame/Constant.java
@@ -0,0 +1,8 @@
+package com.sissi.vconfsdk;
+
+/**
+ * Created by Sissi on 2018/9/13.
+ */
+
+public class Constant {
+}
---
+++ app/src/main/java/com/sissi/vconfsdk/frame/RequestBundle.java
@@ -0,0 +1,8 @@
+package com.sissi.vconfsdk.frame;
+
+/**
+ * Created by Sissi on 2018/9/13.
+ */
+
+class RequestBundle {
+}
