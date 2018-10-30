56f94fdbb5fbb0ea37ca79dd61e60c52ce3b5492
xingbiyanshu <xingbiyanshu@gmail.com>
2018-10-30 15:45:40 +0800

    修复监控lambda生命周期失效的问题；

--- app/src/main/java/com/kedacom/vconf/sdk/base/ListenerLifecycleObserver.java
+++ app/src/main/java/com/kedacom/vconf/sdk/base/ListenerLifecycleObserver.java
@@ -24,6 +24,24 @@ class ListenerLifecycleObserver implements DefaultLifecycleObserver {
         ownerEnclosedListeners = new HashMap<>();
     }
 
+    /**尝试监控对象的生命周期。
+     *
+     * 若受监控对象本身即为生命周期拥有者，如androidx.appcompat.app.AppCompatActivity, androidx.fragment.app.Fragment，则监控该对象本身，
+     * 否则尝试监控该对象的直接外部类对象，如:
+     * class LoginActivity extends AppCompatActivity{
+     *  tryObserve(new LoginResultListener(){...})
+     * }
+     * interface LoginResultListener{
+     *     onLoginResult(int resultCode, Object result);
+     * }
+     * 则监控的是LoginResultListener匿名内部类中引用的外部类对象——LoginActivity对象。
+     *
+     * XXX 注意：1、该方法使用了反射，可能随着所使用java版本的变化而失效；
+     *           2、对于受监控对象自身不拥有生命周期的情形，仅尝试监控其直接外部类，若该对象的直接外部类也不是生命周期拥有者则返回尝试监控失败，不做进一步递归（即尝试监控其外部类的外部类...）。
+     *           3、对于受监控对象为lambda的情形，若该lambda表达式内未使用外部类对象或外部类对象的成员则无法获取其外部类对象的引用，返回尝试监控失败。此种情形在实际使用中会较常遇见，使用者需特别留意，评估由此带来的影响。
+     *
+     * @param listener 受监控对象
+     * */
     boolean tryObserve(Object listener){
         KLog.p("%s instanceof LifecycleOwner? %s", listener, listener instanceof LifecycleOwner);
         if (listener instanceof LifecycleOwner){
@@ -52,27 +70,35 @@ class ListenerLifecycleObserver implements DefaultLifecycleObserver {
         return false;
     }
 
+    /**获取对象的外部类对象。*/
     private Object getEncloser(Object obj){
         Class<?> clz = obj.getClass();
         Class<?> enclosingClz = clz.getEnclosingClass();
         Object encloser = null;
         Field enclosingClzRef;
-        try {
-            //for debug
+        //for debug
 //            Field[] fields = clz.getDeclaredFields();
 //            for (Field field : fields){
 //                KLog.p("======= field: %s", field);
 //            }
-            if (null != enclosingClz) { // 内部类
-                enclosingClzRef = clz.getDeclaredField("this$0"); // 外部类在内部类中的引用名称为"this$0"
-            }else { // lambda
-                /*lambda可能持有外部类引用也可能未持有。
-                若lambda内部引用了外部类或外部类的成员则持有否则未持有。
-                持有的情况下外部类在lambda类中的引用名称为"arg$1"*/
-                enclosingClzRef = clz.getDeclaredField("arg$1");
+        if (null != enclosingClz) { // 内部类
+            try {
+                enclosingClzRef = clz.getDeclaredField("this$0");
+            } catch (NoSuchFieldException e) {
+                return null;
             }
-        } catch (NoSuchFieldException e) {
-            return null;
+        }else { // lambda
+            /*lambda可能持有外部类引用也可能未持有。
+            若lambda内部引用了外部类或外部类的成员则持有否则未持有。*/
+            try {
+                enclosingClzRef = clz.getDeclaredField("arg$1");
+            } catch (NoSuchFieldException e) {
+                try {
+                    enclosingClzRef = clz.getDeclaredField("f$0");
+                } catch (NoSuchFieldException e1) {
+                    return null;
+                }
+            }
         }
 
         enclosingClzRef.setAccessible(true);
