5d0b29b498fca3d962df1779ff870d95bd93e0f2
sissi <xingbiyanshu@gmail.com>
2018-11-24 20:34:08 +0800

    优化

---
+++ datacollaborate/src/main/java/com/kedacom/vconf/sdk/datacollaborate/bean/EOpType.java
@@ -0,0 +1,17 @@
+package com.kedacom.vconf.sdk.datacollaborate.bean;
+
+public enum EOpType {
+    DRAW_LINE,
+    DRAW_RECT,
+    DRAW_OVAL,
+    DRAW_PATH,
+    UNDO,
+    REDO,
+    FULLSCREEN_MATRIX,
+    RECT_ERASE,
+    CLEAR_SCREEN,
+    INSERT_PICTURE,
+    DELETE_PICTURE,
+    DRAG_PICTURE,
+    UPDATE_PICTURE,
+}
