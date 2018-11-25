16b9ce7b072cbd0e63af4b64c818582d46d70976
sissi <xingbiyanshu@gmail.com>
2018-11-25 15:48:45 +0800

    优化

--- datacollaborate/src/main/java/com/kedacom/vconf/sdk/datacollaborate/bean/OpPaint.java
+++ datacollaborate/src/main/java/com/kedacom/vconf/sdk/datacollaborate/bean/OpPaint.java
@@ -3,38 +3,13 @@ package com.kedacom.vconf.sdk.datacollaborate.bean;
 
 import androidx.annotation.NonNull;
 
-public abstract class OpPaint /*implements Comparable<OpPaint>*/{
-//    public static final int EOpType.DRAW_LINE = 1;
-//    public static final int EOpType.DRAW_RECT = 2;
-//    public static final int EOpType.DRAW_OVAL = 3;
-//    public static final int EOpType.DRAW_PATH = 4;
-//    public static final int EOpType.UNDO = 5;
-//    public static final int EOpType.REDO = 6;
-//    public static final int EOpType.FULLSCREEN_MATRIX = 7;
-//    public static final int EOpType.RECT_ERASE = 8;
-//    public static final int EOpType.CLEAR_SCREEN = 9;
-//    public static final int EOpType.INSERT_PICTURE = 10;
-//    public static final int EOpType.DELETE_PICTURE = 11;
-//    public static final int EOpType.DRAG_PICTURE = 12;
-//    public static final int EOpType.UPDATE_PICTURE = 13;
+public abstract class OpPaint {
 
     protected EOpType type; // 类型：划线、画圈等。
 
     protected String   confE164;   // 所属会议e164号
     protected String   boardId;    // 画板ID
     protected int      pageId;     // 文档页ID（仅文档模式下有效）
-//    protected int      sn;             // 操作序列号，用来表示操作的先后顺序，越小越靠前。由平台填写。
-
-//    @Override
-//    public int compareTo(OpPaint o) {
-//        if (sn<o.sn){
-//            return -1;
-//        }else if (sn == o.sn){
-//            return 0;
-//        }else{
-//            return 1;
-//        }
-//    }
 
 
     @NonNull
@@ -75,11 +50,4 @@ public abstract class OpPaint /*implements Comparable<OpPaint>*/{
         this.pageId = pageId;
     }
 
-//    public int getSn() {
-//        return sn;
-//    }
-//
-//    public void setSn(int sn) {
-//        this.sn = sn;
-//    }
 }
