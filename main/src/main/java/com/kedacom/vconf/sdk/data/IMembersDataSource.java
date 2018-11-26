37c6f04aa5a6f468fa540500aa8679570eb33548
xingbiyanshu <xingbiyanshu@gmail.com>
2018-11-26 10:44:00 +0800

    优化

--- main/src/main/java/com/kedacom/vconf/sdk/data/IMembersDataSource.java
+++
@@ -1,8 +0,0 @@
-package com.kedacom.vconf.sdk.data;
-
-import com.kedacom.vconf.sdk.base.IResponseListener;
-
-public interface IMembersDataSource {
-
-    void getMembers(IResponseListener resultListener);
-}
