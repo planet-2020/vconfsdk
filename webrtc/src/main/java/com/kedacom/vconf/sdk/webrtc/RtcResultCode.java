947e01c8115a68932ec5d03642dd0fb14a93adeb
xingbiyanshu <xingbiyanshu@gmail.com>
2020-01-14 14:29:39 +0800

    [add] 会议审批错误码，人数上限错误码

--- webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/RtcResultCode.java
+++ webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/RtcResultCode.java
@@ -9,20 +9,27 @@ import com.google.common.collect.HashBiMap;
  */
 public final class RtcResultCode {
     static final int OK = 0; // 成功
-    public static final int IncorrectConfPassword = 1; // 会议密码错误
-    public static final int NetworkUnreachable = 2; // 网络不可达
-    static final int LoginedAlready = 3; // 已注册（重复注册）
+    public static final int Failed = -1; // 未知错误
+    public static final int NetworkUnreachable = 1; // 网络不可达
+    static final int LoginedAlready = 2; // 已注册（重复注册）
+    public static final int UnknownServerAddress = 3; // 未知服务器
+
     static final int ConfOK = 100; // 创会、查询会议等操作成功
-    public static final int ReachConfereeNumLimit = 101; // 与会方人数达上限（RTC最多8方）
-    public static final int InstantConfDenied = 102; // 平台会议审批功能已开启，不能创建即时会议，只能预约创会。
+    public static final int IncorrectConfPassword = 101; // 会议密码错误
+    public static final int ReachConfereeNumLimit = 102; // 与会方人数达上限（RTC最多8方）
+    public static final int InstantConfDenied = 103; // 平台会议审批功能已开启，不能创建即时会议，只能预约创会。
+    public static final int NotLoginedYet = 104; // 尚未成功登录
 
     private static BiMap<Integer, Integer> errorCodeMap = HashBiMap.create();
     static {
         errorCodeMap.put(100, OK);
+        errorCodeMap.put(72, UnknownServerAddress);
         errorCodeMap.put(73, NetworkUnreachable);
         errorCodeMap.put(77, LoginedAlready);
+
         errorCodeMap.put(1000, ConfOK);
         errorCodeMap.put(39, ReachConfereeNumLimit);
+        errorCodeMap.put(40, NotLoginedYet);
         errorCodeMap.put(99997, InstantConfDenied);
     }
 
