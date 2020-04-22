e817b10974e5cb004be28e2f9cf2d11e94228f17
xingbiyanshu <xingbiyanshu@gmail.com>
2020-04-22 11:30:57 +0800

    [add] 添加gson适配器接口

--- utils/src/main/java/com/kedacom/vconf/sdk/utils/json/Kson.java
+++ utils/src/main/java/com/kedacom/vconf/sdk/utils/json/Kson.java
@@ -1,6 +1,8 @@
 package com.kedacom.vconf.sdk.utils.json;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Map;
 
 /**
  * Created by Sissi on 2019/7/31
@@ -8,7 +10,15 @@ import com.google.gson.Gson;
  * json 封装类
  */
 public final class Kson {
-    private static Gson gson = new Gson();
+    private static GsonBuilder gsonBuilder = new GsonBuilder();
+    private static Gson gson = gsonBuilder.create();
+
+    public static <T> void registerAdapters(Map<Class<T>, Object> adapterMap){
+        for (Map.Entry<Class<T>, Object> entry : adapterMap.entrySet()) {
+            gsonBuilder.registerTypeAdapter(entry.getKey(), entry.getValue());
+        }
+        gson = gsonBuilder.create();
+    }
 
     public static String toJson(Object obj){
         return gson.toJson(obj);
