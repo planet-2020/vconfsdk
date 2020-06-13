e369e43107b3d41abed150ce1fcf3cd4f1d46a0e
xingbiyanshu <xingbiyanshu@gmail.com>
2020-06-13 12:24:59 +0800

    [fix, opt] 修复了调用组件阻塞式接口时timeout不准的问题；去掉了synchronized机制；优化消息标志

--- amulet/src/main/java/com/kedacom/vconf/sdk/amulet/SessionFairy.java
+++ amulet/src/main/java/com/kedacom/vconf/sdk/amulet/SessionFairy.java
@@ -11,7 +11,6 @@ import android.util.SparseIntArray;
 import com.kedacom.vconf.sdk.utils.json.Kson;
 import com.kedacom.vconf.sdk.utils.log.KLog;
 
-import java.util.Arrays;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
@@ -27,26 +26,38 @@ final class SessionFairy implements IFairy.ISessionFairy{
     private Set<Session> sessions = new LinkedHashSet<>();
     private final Object sessionsLock = new Object();
 
-    private static final int MSG_ID_START_SESSION = 100;
-    private static final int MSG_ID_REQUEST_TIMEOUT = 999;
+    private static final int MSG_ID_FinDueToNoRsp = 200;
+    private static final int MSG_ID_SESSION_TIMEOUT = 999;
 
     private static HandlerThread reqThread = new HandlerThread("reqThr", Process.THREAD_PRIORITY_BACKGROUND);
     static {
         reqThread.start();
     }
 
-    private Handler uiHandler = new Handler(Looper.getMainLooper());
-
     private Handler reqHandler = new Handler(reqThread.getLooper()) {
         @Override
         public void handleMessage(Message msg) {
-            if (msg.what == MSG_ID_START_SESSION) {
-                startSession((int) msg.obj);
-            }else if (msg.what == MSG_ID_REQUEST_TIMEOUT) {
-                timeout((int) msg.obj);
+        }
+    };
+
+    private Handler uiHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == MSG_ID_SESSION_TIMEOUT) {
+                Session s = (Session) msg.obj;
+                s.state = Session.END;
+                sessions.remove(s);
+                Log.d(TAG, String.format("%s <-~- timeout", s.id));
+                s.listener.onTimeout(s.reqName, s.reqSn, s.reqPara);
+            }else if (msg.what == MSG_ID_FinDueToNoRsp) {
+                Session s = (Session) msg.obj;
+                sessions.remove(s);
+                Log.d(TAG, String.format("%s <-~- no response", s.id));
+                s.listener.onFinDueToNoRsp(s.reqName, s.reqSn, s.reqPara);
             }
         }
     };
+
 
     @Override
     public void setCrystalBall(ICrystalBall crystalBall) {
@@ -81,13 +92,43 @@ final class SessionFairy implements IFairy.ISessionFairy{
         }
 
         Session s = new Session(listener, reqSn, reqName, reqPara,magicBook.getTimeout(reqName) * 1000, magicBook.getRspSeqs(reqName));
-        synchronized (sessionsLock) {
-            s.state = Session.READY;
-            sessions.add(s);
+        sessions.add(s);
+
+        // 用户参数转换为底层方法需要的参数
+        Object[] paras = magicBook.userPara2MethodPara(s.reqPara, magicBook.getParaClasses(s.reqName));
+        StringBuilder sb = new StringBuilder();
+        for (Object para : paras) {
+            sb.append(para).append(", ");
+        }
+        String methodName = magicBook.getMethod(s.reqName);
+
+        Log.d(TAG, String.format("%s -~-> %s(%s) \nparas={%s}", s.id, s.reqName, methodName, sb));
+
+        if (null == s.rspSeqs || 0 == s.rspSeqs.length) {
+            s.state = Session.END;
             Message msg = Message.obtain();
-            msg.what = MSG_ID_START_SESSION;
-            msg.obj = s.id;
-            reqHandler.sendMessage(msg);
+            msg.what = MSG_ID_FinDueToNoRsp;
+            msg.obj = s;
+            uiHandler.sendMessage(msg);
+        }else {
+            s.state = Session.WAITING; // 请求已发出正在等待响应
+            // 启动超时
+            Message msg = Message.obtain();
+            msg.what = MSG_ID_SESSION_TIMEOUT;
+            msg.obj = s;
+            uiHandler.sendMessageDelayed(msg, s.timeoutVal);
+        }
+
+        if (null != crystalBall) {
+            reqHandler.post(() -> {
+                if (null != crystalBall) {
+                    // 调用底层业务组件接口
+                    crystalBall.spell(magicBook.getMethodOwner(s.reqName),
+                            methodName,
+                            paras,
+                            magicBook.getParaClasses(s.reqName));
+                }
+            });
         }
 
         return true;
@@ -96,87 +137,28 @@ final class SessionFairy implements IFairy.ISessionFairy{
 
     @Override
     public boolean cancelReq(int reqSn) {
-        synchronized (sessionsLock) {
-            for (Session s : sessions) {
-                if (reqSn == s.reqSn) {
-                    s.state = Session.CANCELED;
+        for (Session s : sessions) {
+            if (reqSn == s.reqSn) {
+                s.state = Session.CANCELED;
+                uiHandler.removeMessages(MSG_ID_SESSION_TIMEOUT, s);
+                uiHandler.removeMessages(MSG_ID_FinDueToNoRsp, s);
 
-                    String methodName = magicBook.getMethod(s.reqName);
-                    Log.d(TAG, String.format("%s-/-> Cancel %s / %s", s.id, s.reqName, methodName));
-
-                    // 用户很有可能在onMsg回调中调用cancelReq（onMsg回调到上层的onRsp然后用户在onRsp中cancelReq），
-                    // 然而onMsg中我们正在遍历sessions，所以我们不直接在此处remove而是post到req线程
-                    reqHandler.post(() -> {
-                        synchronized (sessionsLock) {
-                            s.state = Session.END;
-                            boolean success = sessions.remove(s);
-                            if (!success) {
-                                KLog.p(KLog.ERROR, "Where is the canceled session %s? Who moved my cheese!?", s.id);
-                            }
-                        }
-                    });
-                    return true;
-                }
+                Log.d(TAG, String.format("%s -~-x %s", s.id, s.reqName));
+                // 用户很有可能在onMsg回调中调用cancelReq（onMsg回调到上层的onRsp然后用户在onRsp中cancelReq），
+                // 然而onMsg中我们正在遍历sessions，所以我们延迟删除
+                uiHandler.post(() -> {
+                    if (Session.CANCELED == s.state) {
+                        s.state = Session.END;
+                        sessions.remove(s);
+                    }else{
+                        KLog.p(KLog.ERROR, "canceled session %s gone? something must be wrong!", s.id);
+                    }
+                });
+                return true;
             }
-
-            return false;
-        }
-    }
-
-
-
-    /**
-     * 启动会话
-     * */
-    private void startSession(int sid){
-        if (null == crystalBall){
-            KLog.p(KLog.ERROR, "no crystalBall");
-            return;
-        }
-        Session s = findSession(sid, Session.READY);
-        if (null == s){
-            KLog.p(KLog.ERROR, "no such session with id %s and state in %s", sid, Session.READY);
-            return;
         }
 
-        // 用户参数转换为底层方法需要的参数
-        Object[] paras = magicBook.userPara2MethodPara(s.reqPara, magicBook.getParaClasses(s.reqName));
-        StringBuffer sb = new StringBuffer();
-        for (Object para : paras) {
-            sb.append(para).append(", ");
-        }
-        String methodName = magicBook.getMethod(s.reqName);
-
-        Log.d(TAG, String.format("%s-~-> %s(%s) | session state = START \nparas={%s}", s.id, s.reqName, methodName, sb));
-
-        // 调用底层业务组件接口
-        crystalBall.spell(magicBook.getMethodOwner(s.reqName),
-                methodName,
-                paras,
-                magicBook.getParaClasses(s.reqName));
-
-        synchronized (sessionsLock) {
-            Session session = findSession(sid, Session.READY);
-            if (null == session){
-                KLog.p(KLog.ERROR, "no such session with id %s and state in %s", sid, Session.READY);
-                return;
-            }
-            if (null == session.rspSeqs || 0 == session.rspSeqs.length) {
-                session.state = Session.END;
-                sessions.remove(session);
-                Log.d(TAG, String.format("%s<-~- | session state = END(no response), req=%s", session.id, session.reqName));
-                uiHandler.post(() -> session.listener.onFinDueToNoRsp(session.reqName, session.reqSn, session.reqPara));
-                return;
-            }
-
-            session.state = Session.WAITING; // 请求已发出正在等待响应
-
-            // 启动超时
-            Message msg = Message.obtain();
-            msg.what = MSG_ID_REQUEST_TIMEOUT;
-            msg.obj = session.id;
-            reqHandler.sendMessageDelayed(msg, session.timeoutVal);
-        }
+        return false;
     }
 
 
@@ -227,19 +209,15 @@ final class SessionFairy implements IFairy.ISessionFairy{
                         s.reqName, s.reqSn, s.reqPara);
 
                 if (bConsumed) {
-                    if (Session.WAITING == s.state || Session.RECVING == s.state) {
-                        s.candidates = candidates; // 更新候选序列
-                        if (gotLast) {
-                            reqHandler.removeMessages(MSG_ID_REQUEST_TIMEOUT, s.id); // 移除定时器
-                            s.state = Session.END; // 已获取到所有期待的响应，该会话结束
-                            sessions.remove(s);
-                            Log.d(TAG, String.format("%s<-~- %s(%s) | session state = END, req=%s \n%s", s.id, msgName, msgId, s.reqName, msgContent));
-                        } else {
-                            s.state = Session.RECVING; // 已收到响应，继续接收后续响应
-                            Log.d(TAG, String.format("%s<-~- %s(%s) | session state = RECVING, req=%s \n%s", s.id, msgName, msgId, s.reqName, msgContent));
-                        }
-                    }else{
-                        Log.d(TAG, String.format("%s<-~- %s(%s) | session state = END(interrupted), req=%s \n%s", s.id, msgName, msgId, s.reqName, msgContent));
+                    s.candidates = candidates; // 更新候选序列
+                    if (gotLast) {
+                        uiHandler.removeMessages(MSG_ID_SESSION_TIMEOUT, s); // 移除定时器
+                        s.state = Session.END; // 已获取到所有期待的响应，该会话结束
+                        sessions.remove(s);
+                        Log.d(TAG, String.format("%s <-~-o %s(%s) \n%s", s.id, msgName, msgId, msgContent));
+                    } else {
+                        s.state = Session.RECVING; // 已收到响应，继续接收后续响应
+                        Log.d(TAG, String.format("%s <-~- %s(%s) \n%s", s.id, msgName, msgId, msgContent));
                     }
                 }
 
@@ -250,60 +228,6 @@ final class SessionFairy implements IFairy.ISessionFairy{
         return false;
     }
 
-
-
-    /**
-     * 会话超时
-     * */
-    private void timeout(int sid){
-        synchronized (sessionsLock) {
-            Session s = findSession(sid, Session.WAITING, Session.RECVING);
-            if (null == s) {
-                KLog.p(KLog.ERROR, "no such session with id %s and state in %s|%s", sid, Session.WAITING, Session.RECVING);
-                return;
-            }
-
-            s.state = Session.TIMEOUT;
-
-            // 通知用户请求超时
-            uiHandler.post(() -> {
-                synchronized (sessionsLock) {
-                    Session session = findSession(s.id, Session.TIMEOUT);
-                    if (null == session) {
-                        KLog.p(KLog.ERROR, "no such session with id %s and state in %s", sid, Session.TIMEOUT);
-                        return;
-                    }
-                    session.state = Session.END;
-                    sessions.remove(session);
-                    Log.d(TAG, String.format("%s<-~- | session state = END(timeout). req=%s", session.id, session.reqName));
-                    session.listener.onTimeout(session.reqName, session.reqSn, session.reqPara);
-                }
-            });
-        }
-    }
-
-    /**
-     * 查找会话
-     * @param sid 会话id
-     * @param states 会话所在状态列表
-     * */
-    private Session findSession(int sid, int... states){
-        synchronized (sessionsLock) {
-            for (Session s : sessions) {
-                if (sid == s.id) {
-                    if (null != states && states.length == 0){
-                        return s;
-                    }
-                    for (int state : states) {
-                        if (state == s.state) {
-                            return s;
-                        }
-                    }
-                }
-            }
-        }
-        return null;
-    }
 
 
     /**
@@ -321,11 +245,8 @@ final class SessionFairy implements IFairy.ISessionFairy{
 
         private int state;  // 会话状态
         private static final int IDLE = 0;  // 空闲。初始状态
-//        private static final int BLOCKING = 1;  // 阻塞。若存在未完成的同类会话（请求名称相同）则当前会话被阻塞直到同类会话结束。
-        private static final int READY = 2;  // 就绪。请求加入队列待发送。
         private static final int WAITING = 3; // 等待。请求发送以后，收到第一条响应之前。
         private static final int RECVING = 4; // 接收。收到第一条响应后，收到最后一条响应之前。
-        private static final int TIMEOUT = 5; // 超时。限定时间内未等到最后一条响应。
         private static final int CANCELED = 6; // 被取消。会话结束前用户取消了该会话。
         private static final int END = 7;   // 结束。
 
@@ -347,21 +268,6 @@ final class SessionFairy implements IFairy.ISessionFairy{
             state = IDLE;
         }
 
-
-        @Override
-        public String toString() {
-            return "Session{" +
-                    "id=" + id +
-                    ", listener=" + listener.hashCode() +
-                    ", reqSn=" + reqSn +
-                    ", reqName='" + reqName + '\'' +
-                    ", reqPara=" + Arrays.toString(reqPara) +
-                    ", timeoutVal=" + timeoutVal +
-                    ", rspSeqs=" + Arrays.toString(rspSeqs) +
-                    ", candidates=" + candidates +
-                    ", state=" + state +
-                    '}';
-        }
     }
 
 }
