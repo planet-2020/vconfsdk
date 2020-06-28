94ecb60ecf5fa922b80357133187f2e3e21fb11f
xingbiyanshu <xingbiyanshu@gmail.com>
2020-06-28 18:53:28 +0800

    [opt] 适配

--- amulet/src/main/java/com/kedacom/vconf/sdk/amulet/IResultListener.java
+++ amulet/src/main/java/com/kedacom/vconf/sdk/amulet/IResultListener.java
@@ -13,15 +13,15 @@ public interface IResultListener extends ILifecycleOwner {
      * */
     default void onArrive(boolean bSuccess){}
 
-    /**请求成功。
+    /**成功。
      * @param result 请求对应的结果。具体定义在各自模块查阅*/
     default void onSuccess(Object result){}
 
-    /**请求失败。
+    /**失败。
      * @param errorCode 错误码。具体定义在各自模块查阅
      * */
     default void onFailed(int errorCode){}
 
-    /**请求超时*/
+    /**超时*/
     default void onTimeout(){}
 }
