fc1911bf8ee44bb1a6252913e20297ea9da67b34
xingbiyanshu <xingbiyanshu@gmail.com>
2020-08-04 14:58:34 +0800

    [fix] 修复启动超时过短导致无响应的问题

--- components/base/src/main/java/com/kedacom/vconf/sdk/base/startup/Msg.java
+++ components/base/src/main/java/com/kedacom/vconf/sdk/base/startup/Msg.java
@@ -51,7 +51,7 @@ enum Msg {
                     String.class, // 终端软件版本号
             },
             rspSeq = "StartMtBaseRsp", // 下层实际并不会抛消息上来，超时是必然。
-            timeout = 1.5
+            timeout = 2
             )
     StartMtBase,
 
