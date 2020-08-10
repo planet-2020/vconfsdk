77fd6c4f97deafed682ac219796cac116ed98183
xingbiyanshu <xingbiyanshu@gmail.com>
2020-08-10 13:16:12 +0800

    [add] 添加VerificationAttemptsReachLimit错误码

--- components/alirtc/src/main/java/com/kedacom/vconf/sdk/alirtc/AliRtcResultCode.java
+++ components/alirtc/src/main/java/com/kedacom/vconf/sdk/alirtc/AliRtcResultCode.java
@@ -14,6 +14,7 @@ public final class AliRtcResultCode {
     public static final int HasLoggedOnOtherDeviceAndCannotBeKickedOff = 12;   // 已在其他设备上登录且不能被抢登
 
     public static final int IncorrectConfPassword = 20;  // 会议密码错误
+    public static final int VerificationAttemptsReachLimit = 21;  // 密码验证次数达到上限
 
 
     /**
@@ -23,22 +24,12 @@ public final class AliRtcResultCode {
     private static Table<Msg, Object, Integer> resultCodes = HashBasedTable.create();
 
     static {
-//        resultCodes.put(Msg.Login, 100, OK);
-//        resultCodes.put(Msg.Login, 72, UnknownServerAddress);
-//        resultCodes.put(Msg.Login, 73, NetworkUnreachable);
-//        resultCodes.put(Msg.Login, 77, MultipleRegistration);
-//        resultCodes.put(Msg.Logout, 90, OK);
-//        resultCodes.put(Msg.CreateConf, 1000, OK);
-//        resultCodes.put(Msg.CreateConf, 99997, InstantConfDenied);
-        resultCodes.put(Msg.JoinConfRsp, 30337, IncorrectConfPassword); // 密码为空时报这个
-        resultCodes.put(Msg.JoinConfRsp, 30327, IncorrectConfPassword);
         resultCodes.put(Msg.LoginStateChanged, 100, LoginSuccess);
         resultCodes.put(Msg.LoginStateChanged, 90, LogoutSuccess);
-        resultCodes.put(Msg.LoginStateChanged, 161, HasLoggedOnOtherDeviceAndCannotBeKickedOff); // 密码为空时报这个
-//        resultCodes.put(Msg.JoinConf, 39, ReachConfereeNumLimit);
-//        resultCodes.put(Msg.AcceptInvitation, 39, ReachConfereeNumLimit);
-//        resultCodes.put(Msg.QueryConfInfo, 1000, OK);
-//        resultCodes.put(Msg.LoginStateChanged, 100, OK);
+        resultCodes.put(Msg.LoginStateChanged, 161, HasLoggedOnOtherDeviceAndCannotBeKickedOff);
+        resultCodes.put(Msg.JoinConfRsp, 30337, IncorrectConfPassword); // 密码为空时报这个
+        resultCodes.put(Msg.JoinConfRsp, 30327, IncorrectConfPassword);
+        resultCodes.put(Msg.JoinConfRsp, 30336, VerificationAttemptsReachLimit);
     }
 
     static int trans(@NonNull Msg msg, Object rawResultCode){
