d74fec95ecdc0775b2f3e2f3a7aea69a0a6a5613
xingbiyanshu <xingbiyanshu@gmail.com>
2020-08-21 16:08:28 +0800

    [fix] 修复注释错误

--- components/alirtc/src/main/java/com/kedacom/vconf/sdk/alirtc/bean/ConfAboutToEnd.java
+++ components/alirtc/src/main/java/com/kedacom/vconf/sdk/alirtc/bean/ConfAboutToEnd.java
@@ -2,7 +2,7 @@ package com.kedacom.vconf.sdk.alirtc.bean;
 
 public class ConfAboutToEnd {
     public String confNum;   //会议号
-    public int leftTime;   // 剩余时长。单位：分钟
+    public int leftTime;   // 剩余时长。单位：秒
     public boolean isExperience;  // 是否为体验会议
 
     public ConfAboutToEnd(String confNum, int leftTime, boolean isExperience) {
