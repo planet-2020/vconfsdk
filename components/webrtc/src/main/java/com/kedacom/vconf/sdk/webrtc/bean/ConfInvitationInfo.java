f9a55ad6e9fabe117dda6cc5aeeeaef04e43b4c7
xingbiyanshu <xingbiyanshu@gmail.com>
2020-08-25 10:41:21 +0800

    [fix] 修正注释

--- components/webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/bean/ConfInvitationInfo.java
+++ components/webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/bean/ConfInvitationInfo.java
@@ -8,7 +8,7 @@ public final class ConfInvitationInfo {
     public String e164;     // 邀请方e164
     public String alias;    // 邀请方别名
     public boolean bP2p;    // 是否为点对点。true 点对点，false 多点
-    public int bitRate;     // 会议码率。单位: KB/s
+    public int bitRate;     // 会议码率。单位: kb/s
 
     public ConfInvitationInfo(String e164, String alias, boolean bP2p, int bitRate) {
         this.e164 = e164;
