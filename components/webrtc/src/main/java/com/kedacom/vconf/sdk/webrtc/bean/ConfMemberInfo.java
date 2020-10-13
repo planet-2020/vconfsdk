1b62211be32f08d6e676611dbcb77c6a2cfea5e0
xingbiyanshu <xingbiyanshu@gmail.com>
2020-10-13 11:13:06 +0800

    [opt] publish使用gradle android plugin自带的机制

--- components/webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/bean/ConfMemberInfo.java
+++ components/webrtc/src/main/java/com/kedacom/vconf/sdk/webrtc/bean/ConfMemberInfo.java
@@ -1,7 +1,5 @@
 package com.kedacom.vconf.sdk.webrtc.bean;
 
-import javax.annotation.Nullable;
-
 /**
  * 与会成员信息
  * Created by Sissi on 2019/12/5
@@ -15,7 +13,7 @@ public final class ConfMemberInfo {
     /**
      * 与会成员信息，至少一项不为空，可通过该项呼叫到该成员。
      * */
-    public ConfMemberInfo(@Nullable String e164, @Nullable String moid, @Nullable String alias, @Nullable String email) {
+    public ConfMemberInfo(String e164, String moid, String alias, String email) {
         this.e164 = e164;
         this.moid = moid;
         this.alias = alias;
