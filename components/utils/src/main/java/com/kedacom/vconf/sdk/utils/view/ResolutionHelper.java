c56439d4d914526ec1ec61c6f114edcaa3abc09c
xingbiyanshu <xingbiyanshu@gmail.com>
2020-11-12 13:08:35 +0800

    [opt] ResolutionHelper

--- components/utils/src/main/java/com/kedacom/vconf/sdk/utils/view/ResolutionHelper.java
+++ components/utils/src/main/java/com/kedacom/vconf/sdk/utils/view/ResolutionHelper.java
@@ -2,78 +2,76 @@ package com.kedacom.vconf.sdk.utils.view;
 
 public final class ResolutionHelper {
 
-    public static final int SCALE_TO_FIT = 0;
+    /**
+     * 维持比例适应目标窗口。处理后的矩形(sw/sh)至少一边贴合目标矩形(dw/dh)且在目标矩形内或重合，关系如下所示：
+     *  |------ dw ------|
+     *  ------------------  -
+     *  |            |   |  |
+     *  |            |   |dh,sh
+     *  |            |   |  |
+     *  |-----------------  -
+     *  |--- sw -----|
+     */
     public static final int SCALE_ASPECT_FIT = 1;
+
+    /**
+     * 维持比例填充目标窗口。处理后的矩形(sw/sh)至少一边贴合目标矩形(dw/dh)且在目标矩形外或重合，关系如下所示：
+     *  |------ sw ------|
+     *  ------------------  -
+     *  |            |   |  |
+     *  |            |   |dh,sh
+     *  |            |   |  |
+     *  |-----------------  -
+     *  |--- dw -----|
+     * */
     public static final int SCALE_ASPECT_FILL = 2;
 
     /**
      * 根据目标宽高及缩放策略对源宽高进行缩放处理，返回处理结果。
-     * @param strategy {@link #SCALE_TO_FIT},{@link #SCALE_ASPECT_FIT},{@link #SCALE_ASPECT_FILL}
-     * @return float[] result. result[0]为处理后的宽，result[1]为处理后的高, result[2]为宽的缩放比，result[3]为高的缩放比。
+     * @param strategy {@link #SCALE_ASPECT_FIT},{@link #SCALE_ASPECT_FILL}
+     * @return int[] result. result[0]为处理后的宽，result[1]为处理后的高。
      * */
-    public static float[] adjust(int srcWidth, int srcHeight, int dstWidth, int dstHeight, int strategy){
-        float width=srcWidth, height=srcHeight, scaleW=1, scaleH=1;
-        if (SCALE_ASPECT_FIT == strategy){
-            if (width*dstHeight > dstWidth*height){ // 源分辨率对比目标分辨率显得比较宽胖
-                // 维持比例缩放使得宽刚好贴合目标宽
-                scaleW = scaleH = dstWidth/width;
-                width = dstWidth;
-                height *= scaleH;
-            }else { // 源分辨率对比目标分辨率显得比较高瘦
-                // 维持比例缩放使得高刚好贴合目标高
-                scaleW = scaleH = dstHeight/height;
-                height = dstHeight;
-                width *= scaleW;
+    public static int[] adjust(int srcWidth, int srcHeight, int dstWidth, int dstHeight, int strategy){
+        if (srcWidth == dstWidth && srcHeight == dstHeight){
+            return new int[]{srcWidth, srcHeight};
+        }
+        int[] result;
+        if (SCALE_ASPECT_FIT == strategy || SCALE_ASPECT_FILL == strategy){
+            boolean fatter = isFatter(srcWidth, srcHeight, dstWidth, dstHeight);
+            if ((fatter && SCALE_ASPECT_FIT == strategy)
+                    || (!fatter && SCALE_ASPECT_FILL == strategy)){
+                result = alignWidth(srcWidth, srcHeight, dstWidth);
+            }else {
+                result = alignHeight(srcWidth, srcHeight, dstHeight);
             }
-        }else if (SCALE_ASPECT_FILL == strategy){
-            if (width*dstHeight > dstWidth*height){ // 源分辨率对比目标分辨率显得比较宽胖
-                // 维持比例缩放使得高刚好贴合目标高
-                scaleW = scaleH = dstHeight/height;
-                height = dstHeight;
-                width *= scaleW;
-            }else { // 源分辨率对比目标分辨率显得比较高瘦
-                // 维持比例缩放使得宽刚好贴合目标宽
-                scaleW = scaleH = dstWidth/width;
-                width = dstWidth;
-                height *= scaleH;
-            }
-        }else if (SCALE_TO_FIT == strategy){
-            scaleW = dstWidth/width;
-            scaleH = dstHeight/height;
-            width = dstWidth;
-            height = dstHeight;
+        }else {
+            result = new int[]{srcWidth, srcHeight};
         }
 
-        return new float[]{width, height, scaleW, scaleH};
+        return result;
     }
 
+    /**
+     * 源窗口是否比目标窗口更“胖”，即宽高比更大。
+     * */
+    private static boolean isFatter(int srcWidth, int srcHeight, int dstWidth, int dstHeight){
+        return srcWidth*dstHeight > dstWidth*srcHeight;
+    }
 
     /**
-     * 将源宽高进行最小缩放处理以至符合目标宽高比
-     * @param shrink true,进行缩小处理以满足目标宽高比，false,进行放大处理以满足目标宽高比。
-     * @param limitedHeight 高度上限。转换出的高度不能超过该限制但可以小于
-     * @return float[] result. result[0]为处理后的宽，result[1]为处理后的高, result[2]为宽的缩放比，result[3]为高的缩放比。
+     * （按比例缩放以）对齐宽
+     * @return int[] result. result[0]为处理后的宽，result[1]为处理后的高。
      * */
-    public static float[] adjust(int srcWidth, int srcHeight, float dstRatio, int limitedHeight, boolean shrink){
-        float width=srcWidth, height=srcHeight, scaleW=1, scaleH=1;
-        if ((width > height*dstRatio && shrink) // 如果源比较宽且是缩小策略
-                || (width < height*dstRatio && !shrink) // 或者源比较高且是放大策略
-        ){
-            // 则对宽进行缩放处理以匹配目标宽高比
-            width = (int) (height*dstRatio);
-        }else {
-            // 否则对高进行缩放处理以匹配目标宽高比
-            height = (int) (width/dstRatio);
-        }
-        if (height>limitedHeight){
-            // 超出限制则等比缩小
-            float scaleFactor = limitedHeight/height;
-            width *=scaleFactor;
-            height *=scaleFactor;
-        }
-        scaleW = width/srcWidth;
-        scaleH = height/srcHeight;
-        return new float[]{width, height, scaleW, scaleH};
+    private static int[] alignWidth(int srcWidth, int srcHeight, int dstWidth){
+        return new int[]{dstWidth, (int) (srcHeight*((float)dstWidth/srcWidth))};
+    }
+
+    /**
+     * （按比例缩放以）对齐高
+     * @return int[] result. result[0]为处理后的宽，result[1]为处理后的高。
+     * */
+    private static int[] alignHeight(int srcWidth, int srcHeight, int dstHeight){
+        return new int[]{(int) (srcWidth*((float)dstHeight/srcHeight)), dstHeight};
     }
 
 }
